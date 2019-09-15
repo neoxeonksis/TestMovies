2d22114b638dd0b616dab118c7adf67c200e020e
Jorge <47952708+J4ackSk3le7on@users.noreply.github.com>
2019-09-14 21:58:08 -0500

    prueba 2

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -18,9 +20,36 @@ class HomePage extends StatelessWidget {
           )
         ],
       ),
-      body: SafeArea(
-        child: Text("Hola Mundo!!!!!!"),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            _swiperTarjetas()
+          ],
+        ),
       )
     );
   }
+
+  Widget _swiperTarjetas() {
+
+    return Container(
+      padding: EdgeInsets.only(top: 10.0),
+      width: double.infinity,
+      height: 300.0,
+      child: Swiper(
+        layout: SwiperLayout.STACK,
+        itemWidth: 200.0,
+          itemBuilder: (BuildContext context,int index){
+            return new Image.network("http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,);
+          },
+          itemCount: 3,
+          pagination: new SwiperPagination(),
+          control: new SwiperControl(),
+        ),
+    );
+
+  }
+
+
 }
