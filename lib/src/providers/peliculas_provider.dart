a09d02ffabaaff3f18affb668eedf234141ec299
Jorge <47952708+J4ackSk3le7on@users.noreply.github.com>
2019-10-19 18:47:31 -0500

    work

---
+++ lib/src/providers/peliculas_provider.dart
@@ -0,0 +1,29 @@
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
+
+import 'package:peliculas/src/models/pelicula_model.dart';
+
+class PeliculasProvider {
+
+  String _apikey   = " dc5ee51df86868cb2868a3d0ab1971c2 ";
+  String _url      = " api.themoviedb.org ";
+  String _language = " es-ES ";
+
+  Future< List<Pelicula>> getEnCines() async {
+
+    final url = Uri.https(_url, "3/movie/now_playing",{
+      "api_key"   : _apikey,
+      "language"  : _language
+    });
+
+    final resp = await http.get( url );
+    final decodedData = json.decode(resp.body);
+
+    final peliculas = new Peliculas.fromJsonList(decodedData["results"]);
+
+    return peliculas.items;
+
+  }
+
+}
